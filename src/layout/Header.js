fdc72056580b52145012088f7251a7d3147d8c5f
ramneek008 <singhramneek274@gmail.com>
2020-09-28 18:43:47 +0530

    Navbar Toggler

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -6,24 +6,30 @@ import {Link} from 'react-router-dom';
 import {UserContext} from '../context/UserContext';
 
 const Header = () => {
+    const context = useContext(UserContext);
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return(
         <Navbar color="info" light expand="md">
             <NavbarBrand><Link to="/" className="text-white">Github UserBook</Link></NavbarBrand>
-            <NavbarToggler />
-            <Collapse navbar>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
                 <Nav className="ml-auto" navbar>
                     <NavItem>
-                        <NavLink className="text-white">
+                        <NavLink tag={Link} to ="/" className="text-white">
                             SignUp
                         </NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink className="text-white">
+                        <NavLink tag={Link} to ="/" className="text-white">
                             SignIn
                         </NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink className="text-white">
+                        <NavLink tag={Link} to ="/" className="text-white">
                             LogOut
                         </NavLink>
                     </NavItem>
